5b18c0913668daf24738a6b8c14c3375547ffff2
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2023-02-22 22:37:00 -0500

    Adding Log In Redux Logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,40 @@
+import { useSelector, useDispatch } from "react-redux";
+
 import classes from "./Header.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { authActions } from "../store/index";
 
 //
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1 className={classes.title}>Redux Auth</h1>
 
       {/* Mid - Large Screen */}
+      {isAuth && (
+        <nav className={classes.navBar}>
+          <ul className={classes.lgMenu}>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
 
-      <nav className={classes.navBar}>
-        <ul className={classes.lgMenu}>
-          <li>
-            <a href="/">My Products</a>
-          </li>
-          <li>
-            <a href="/">My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
       <div className={classes.mobileMenu}>
         <GiHamburgerMenu className={classes.mobileMenuIcon} />
       </div>
