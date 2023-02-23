2094e452f470692a4234f20880739c7c5fe344f0
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2023-02-22 22:51:08 -0500

    Splitting states

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Header.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { authActions } from "../store/index";
+import { authActions } from "../store/auth";
 
 //
 
